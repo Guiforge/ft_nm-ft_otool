753ae8ea5bc71308a31eec87cd85b5ffa0e3d4ec
Unknown <gpouyat@vejja.fr>
2018-04-29 20:29:15 +0200

    [FEATURE]Master: Add ft_getopt

--- srcs/ft_nm/main.c
+++ srcs/ft_nm/main.c
@@ -6,7 +6,7 @@
 /*   By: gpouyat <gpouyat@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/24 09:02:33 by gpouyat           #+#    #+#             */
-/*   Updated: 2018/04/24 19:38:22 by gpouyat          ###   ########.fr       */
+/*   Updated: 2018/04/29 20:24:31 by gpouyat          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,39 @@
 
 #include <mach-o/loader.h>
 
+extern int g_optind;
+
+static int		intern_nm_help()
+{
+	ft_printf("This is beautiful Help\n");
+	return (1);
+}
+
+static int		intern_nm_parse_option(int ac, const char **av)
+{
+	int opt;
+
+	while((opt = ft_getopt(ac, av, "ah")) != -1)
+	{
+		if (opt == 'h')
+			return (intern_nm_help());
+		if (opt == '?')
+			return (1);
+	}
+	return (0);
+}
 
 int main(int ac, const char **av)
 {
 	t_macho_input	input_file;
+	uint32_t		magic = 0;
 
-	(void)ac;
+	if (intern_nm_parse_option(ac, av))
+		return (1);
 	ft_putendl("I am nm");
 	map_file(PROGNAME, av[1], &input_file);
-	uint32_t magic = *(uint32_t *)secure_add(input_file, input_file.data, sizeof(uint32_t));
+	if (secure_add(input_file, input_file.data, sizeof(uint32_t)))
+		magic = *(uint32_t *)input_file.data;
 	if (magic == MH_MAGIC_64)
 		ft_printf("64\n");
 	ft_printf("size:%d, data:%#x\n", input_file.length, input_file.data);
